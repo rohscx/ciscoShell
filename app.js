ad87084a8ab712c017adc45b17d04d9e481b33c7
Foster <Erou.Foster@financialpartners.com>
2020-03-26 09:50:06 -0400

    added methods

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const bootPath = require('./lib/bootPathParser.js');
 const hostName = require('./lib/hostNameParser.js');
 const acaSwitches = require('./import/acaSwitches.js');
 const ntfsFileNameFormatter = require('./lib/ntfsFileNameFormatter.js');
-const {objectKeyFilter,writeFile} = require('nodeutilz');
-const dotenv = require('dotenv').config({path:'./.env'});
+const {
+  objectKeyFilter,
+  writeFile,
+  readFile,
+  asyncRequest,
+  prtgOptions,
+} = require('nodeutilz');
+const dotenv = require('dotenv').config({ path: './.env' });
 
 module.exports = {
-    sshShell,
-    md5Result,
-    bootPath,
-    hostName,
-    objectKeyFilter,
-    dotenv,
-    writeFile,
-    ntfsFileNameFormatter
+  sshShell,
+  md5Result,
+  bootPath,
+  hostName,
+  objectKeyFilter,
+  dotenv,
+  writeFile,
+  ntfsFileNameFormatter,
+  readFile,
+  asyncRequest,
+  prtgOptions,
 };
-
-
-
-
-
-
