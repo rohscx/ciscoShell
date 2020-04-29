e1725a438c6ebbc09a1131644888bc9b4823d392
Foster <Erou.Foster@financialpartners.com>
2020-04-29 10:18:17 -0400

    clean up

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const sshShell = require('./lib/sshShell.js');
 const md5Result = require('./lib/md5ResultParser.js');
 const bootPath = require('./lib/bootPathParser.js');
 const hostName = require('./lib/hostNameParser.js');
-const acaSwitches = require('./import/acaSwitches.js');
+const acaSwitches = require('./import/acaSwitchess.js');
 const ntfsFileNameFormatter = require('./lib/ntfsFileNameFormatter.js');
 const {
-  objectKeyFilter,
-  writeFile,
-  readFile,
-  asyncRequest,
-  prtgOptions,
-  csvFromJson,
-  filterFieldsCsv,
+    objectKeyFilter,
+    writeFile,
+    readFile,
+    asyncRequest,
+    prtgOptions,
+    csvFromJson,
+    filterFieldsCsv,
 } = require('nodeutilz');
 const dotenv = require('dotenv').config({ path: './.env' });
 
 module.exports = {
-  sshShell,
-  md5Result,
-  bootPath,
-  hostName,
-  objectKeyFilter,
-  dotenv,
-  writeFile,
-  ntfsFileNameFormatter,
-  readFile,
-  asyncRequest,
-  prtgOptions,
-  csvFromJson,
-  filterFieldsCsv
+    sshShell,
+    md5Result,
+    bootPath,
+    hostName,
+    objectKeyFilter,
+    dotenv,
+    writeFile,
+    ntfsFileNameFormatter,
+    readFile,
+    asyncRequest,
+    prtgOptions,
+    csvFromJson,
+    filterFieldsCsv,
 };
